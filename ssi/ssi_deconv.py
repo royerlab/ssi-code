ba236714de052d6df0e4155e103200354e90cedb
AhmetCanSolak <ahmetcan.solak@czbiohub.org>
2020-06-16 20:58:44 -0700

    to keep changes

---
+++ ssi/ssi_deconv.py
@@ -0,0 +1,99 @@
+import numpy
+import torch
+import torch.nn.functional as F
+from scipy.signal import convolve2d
+
+from ssi.it_ptcnn import PTCNNImageTranslator
+from ssi.models.psf_convolution import PSFConvolutionLayer
+from ssi.utils.log.log import lprint
+
+
+class SSIDeconvolution(PTCNNImageTranslator):
+    """
+    Pytorch-based CNN image deconvolution
+    """
+
+    def __init__(self, psf_kernel=None, broaden_psf=1, sharpening=0, bounds_loss=0.1, entropy=0, num_channels=1, **kwargs):
+        """
+        Constructs a CNN image translator using the pytorch deep learning library.
+
+        :param normaliser_type: normaliser type
+        :param balance_training_data: balance data ? (limits number training entries per target value histogram bin)
+        :param monitor: monitor to track progress of training externally (used by UI)
+        """
+        super().__init__(**kwargs)
+
+        for i in range(broaden_psf):
+            psf_kernel = numpy.pad(psf_kernel, (1,), mode='constant', constant_values=0)
+            broaden_kernel = numpy.array([[0.095, 0.14, 0.095], [0.11, 0.179, 0.11], [0.095, 0.14, 0.095]])
+            broaden_kernel = broaden_kernel / broaden_kernel.sum()
+            psf_kernel = convolve2d(
+                psf_kernel,
+                broaden_kernel,
+                'same',
+            )
+
+        psf_kernel /= psf_kernel.sum()
+        psf_kernel = psf_kernel.astype(numpy.float32)
+
+        self.psf_kernel = psf_kernel
+        self.psf_kernel_tensor = torch.from_numpy(
+            self.psf_kernel[numpy.newaxis, numpy.newaxis, ...]
+        ).to(self.device)
+
+        self.psfconv = PSFConvolutionLayer(self.psf_kernel, num_channels=num_channels).to(self.device)
+
+        self.enforce_blind_spot = False
+
+        self.sharpening = sharpening
+        self.bounds_loss = bounds_loss
+        self.entropy = entropy
+
+    def _train_loop(self, data_loader, optimizer, loss_function):
+        try:
+            self.model.kernel_continuity_regularisation = False
+        except AttributeError:
+            lprint("Cannot deactivate kernel continuity regularisation")
+
+        super()._train_loop(data_loader, optimizer, loss_function)
+
+    def _additional_losses(self, translated_image, forward_model_image):
+
+        loss = 0
+
+        # Bounds loss:
+        if self.bounds_loss and self.bounds_loss != 0:
+            epsilon = 0 * 1e-8
+            bounds_loss = F.relu(-translated_image - epsilon)
+            bounds_loss += F.relu(translated_image - 1 - epsilon)
+            bounds_loss_value = bounds_loss.mean()
+            lprint(f"bounds_loss_value = {bounds_loss_value}")
+            loss += self.bounds_loss * bounds_loss_value ** 2
+
+        # Sharpen loss_deconvolution:
+        if self.sharpening and self.sharpening != 0:
+            image_for_loss = translated_image
+            num_elements = image_for_loss[0, 0].nelement()
+            sharpening_loss = -torch.norm(image_for_loss, dim=(2, 3), keepdim=True, p=2) / (num_elements ** 2)  # /torch.norm(image_for_loss, dim=(2, 3), keepdim=True, p=1)
+            lprint(f"sharpening loss = {sharpening_loss}")
+            loss += self.sharpening * sharpening_loss.mean()
+
+        # Max entropy loss:
+        if self.entropy and self.entropy != 0:
+            entropy_value = entropy(translated_image)
+            lprint(f"entropy_value = {entropy_value}")
+            loss += -self.entropy * entropy_value
+
+        return loss
+
+    def _forward_model(self, input):
+        return self.psfconv(torch.clamp(input, 0, 1))
+
+
+def entropy(image, normalise=True, epsilon=1e-10, clip=True):
+    if clip:
+        image = torch.clamp(image, 0, 1)
+    image = image / (epsilon + torch.sum(image, dim=(2, 3), keepdim=True)) if normalise else image
+    entropy = -torch.where(image > 0, image * (image + epsilon).log(), image.new([0.0]))
+    entropy_value = entropy.sum(dim=(2, 3), keepdim=True).mean()
+    return entropy_value
---
+++ ssi/utils/io/datasets.py
@@ -0,0 +1,203 @@
+import os
+import zipfile
+from enum import Enum
+from os.path import join, exists
+import gdown
+import numpy
+import skimage
+from imageio import imread
+from numpy.random.mtrand import normal, uniform
+from scipy.ndimage import binary_dilation
+from scipy.signal import convolve
+from scipy.signal import convolve2d
+from skimage.exposure import rescale_intensity
+from skimage.util import random_noise
+
+from ssi.utils.io.folders import get_cache_folder
+from ssi.utils.log.log import lprint
+from ssi.utils.psf.simple_microscope_psf import SimpleMicroscopePSF
+
+
+datasets_folder = join(get_cache_folder(), 'data')
+
+try:
+    os.makedirs(datasets_folder)
+except Exception:
+    pass
+
+
+# Convenience methods to add noise and blur:
+
+def normalise(image):
+    return rescale_intensity(
+        image.astype(numpy.float32), in_range='image', out_range=(0, 1)
+    )
+
+
+def add_poisson_gaussian_noise(image, alpha=5, sigma=0.01, sap=0.0, quant_bits=8, dtype=numpy.float32, clip=True, fix_seed=True
+                               ):
+    if fix_seed:
+        numpy.random.seed(0)
+    rnd = normal(size=image.shape)
+    rnd_bool = uniform(size=image.shape) < sap
+
+    noisy = image + numpy.sqrt(alpha * image + sigma ** 2) * rnd
+    noisy = noisy * (1 - rnd_bool) + rnd_bool * uniform(size=image.shape)
+    noisy = numpy.around((2 ** quant_bits) * noisy) / 2 ** quant_bits
+    noisy = numpy.clip(noisy, 0, 1) if clip else noisy
+    noisy = noisy.astype(dtype)
+    return noisy
+
+
+def add_noise(
+        image, intensity=5, variance=0.01, sap=0.0, dtype=numpy.float32, clip=True
+):
+    numpy.random.seed(0)
+    noisy = image
+    if intensity is not None:
+        noisy = numpy.random.poisson(image * intensity) / intensity
+    noisy = random_noise(noisy, mode="gaussian", var=variance, seed=0, clip=clip)
+    noisy = random_noise(noisy, mode="s&p", amount=sap, seed=0, clip=clip)
+    noisy = noisy.astype(dtype)
+    return noisy
+
+
+def add_blur_2d(image, k=17, sigma=5, multi_channel=False):
+    from numpy import pi, exp, sqrt
+    #  generate a (2k+1)x(2k+1) gaussian kernel with mean=0 and sigma = s
+    probs = [exp(-z * z / (2 * sigma * sigma)) / sqrt(2 * pi * sigma * sigma) for z in range(-k, k + 1)]
+    psf_kernel = numpy.outer(probs, probs)
+
+    def conv(_image):
+        return convolve2d(_image, psf_kernel, mode='same').astype(numpy.float32)
+
+    if multi_channel:
+        image = numpy.moveaxis(image.copy(), -1, 0)
+        return numpy.moveaxis(numpy.stack([conv(channel) for channel in image]), 0, -1), psf_kernel
+    else:
+        return conv(image), psf_kernel
+
+
+def add_microscope_blur_2d(image, dz=0, multi_channel=False):
+    psf = SimpleMicroscopePSF()
+    psf_xyz_array = psf.generate_xyz_psf(dxy=0.406, dz=0.406, xy_size=17, z_size=17)
+    psf_kernel = psf_xyz_array[dz]
+    psf_kernel /= psf_kernel.sum()
+
+    def conv(_image):
+        return convolve2d(_image, psf_kernel, mode='same').astype(numpy.float32)
+
+    if multi_channel:
+        image = numpy.moveaxis(image.copy(), -1, 0)
+        return numpy.moveaxis(numpy.stack([conv(channel) for channel in image]), 0, -1), psf_kernel
+    else:
+        return conv(image), psf_kernel
+
+
+def add_blur_3d(image):
+    psf = SimpleMicroscopePSF()
+    psf_xyz_array = psf.generate_xyz_psf(dxy=0.406, dz=0.406, xy_size=17, z_size=17)
+    psf_kernel = psf_xyz_array
+    psf_kernel /= psf_kernel.sum()
+    return convolve(image, psf_kernel, mode='same'), psf_kernel
+
+
+# Example datasets
+
+
+def lizard():
+    return examples_single.generic_lizard.get_array()
+
+
+def camera():
+    return skimage.data.camera().astype(numpy.float32)
+
+
+def newyork():
+    return examples_single.generic_newyork.get_array()
+
+
+def pollen():
+    return examples_single.generic_pollen.get_array()
+
+
+def scafoldings():
+    return examples_single.generic_scafoldings.get_array()
+
+
+def characters():
+    return 1 - examples_single.generic_characters.get_array()
+
+
+def andromeda():
+    return examples_single.generic_andromeda.get_array()
+
+
+def fibsem(full=False):
+    array = examples_single.scheffer_fibsem.get_array()
+    if not full:
+        array = array[0:1024, 0:1024]
+    return array
+
+
+def dots():
+    image = numpy.random.rand(512, 512) < 0.005  # andromeda()#[256:-256, 256:-256]
+    image = 0.8 * binary_dilation(image).astype(numpy.float32)
+    image[0:256, 0:256] += 0.1
+    image.clip(0, 1)
+    return image
+
+
+class examples_single(Enum):
+    def get_path(self):
+        download_from_gdrive(*self.value, datasets_folder)
+        return join(datasets_folder, self.value[1])
+
+    def get_array(self):
+        array = imread(self.get_path())
+        return array
+
+    # XY natural images (2D monochrome):
+    generic_crowd = ('13UHK8MjhBviv31mAW2isdG4G-aGaNJIj', 'crowd.tif')
+    generic_mandrill = ('1B33ELiFuCV0OJ6IHh7Ix9lvImwI_QkR-', 'mandrill.tif')
+    generic_newyork = ('15Nuu_NU3iNuoPRmpFbrGIY0VT0iCmuKu', 'newyork.png')
+    generic_lizard = ('1GUc6jy5QH5DaiUskCrPrf64YBOLzT6j1', 'lizard.png')
+    generic_pollen = ('1S0o2NWtD1shB5DfGRIqOFxTLOi8cHQD-', 'pollen.png')
+    generic_scafoldings = ('1ZiWhHnkuaQH-BS8B71y00wkN1Ylo38nY', 'scafoldings.png')
+    generic_andromeda = ('1Zl3DtkwUlZSbvpxGILexiIoLW1JOdJh8', 'andromeda.png')
+
+    # Characters (2D monochrome, inverted):
+    generic_characters = ('1ZWkHFI2iddKa9qv6tft4QZlCoDS5fLMK', 'characters.jpg')
+
+
+def download_from_gdrive(
+        id, name, dest_folder=datasets_folder, overwrite=False, unzip=False
+):
+    try:
+        os.makedirs(dest_folder)
+    except Exception:
+        pass
+
+    url = f'https://drive.google.com/uc?id={id}'
+    output_path = join(dest_folder, name)
+    if overwrite or not exists(output_path):
+        lprint(f"Downloading file {output_path} as it does not exist yet.")
+        gdown.download(url, output_path, quiet=False)
+
+        if unzip:
+            lprint(f"Unzipping file {output_path}...")
+            zip_ref = zipfile.ZipFile(output_path, 'r')
+            zip_ref.extractall(dest_folder)
+            zip_ref.close()
+            # os.remove(output_path)
+
+        return output_path
+    else:
+        lprint(f"Not downloading file {output_path} as it already exists.")
+        return None
+
+
+def downloaded_example(substring):
+    for example in examples_single.get_list():
+        if substring in example.value[1]:
+            print(download_from_gdrive(*example.value))
